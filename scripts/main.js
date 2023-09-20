d2d4c974d07993a373765c2c253d29d59c20e923
Huluette <leana.beuvant@gmail.com>
2023-09-20 16:59:23 +0200

    dernier réglage

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,10 @@ const img = document.querySelector('#img');
 // MISE EN PLACE DU TEXTE PORNONCE PAR LE PERSO CHAT //
 
 const mess = document.querySelector('#mess');
+var messageArray = ["Bienvenue sur le CV de ma maîtresse !"];
+var textPosition = 0;
+var speed = 100;
+
 
 btn.addEventListener('click', () => {
     if (img.style.display == 'block') {
@@ -13,9 +17,18 @@ btn.addEventListener('click', () => {
     }
     else {
         img.style.display = 'block';
-        
+
         // MISE EN PLACE DU TEXTE PORNONCE PAR LE PERSO CHAT //
 
         mess.innerHTML = "Bienvenue sur le CV de ma maîtresse !";
+
+        //     typewriter = () => {
+        //         mess.innerHTML = messageArray[0].substring(0, textPosition) + "<span>\u99ae</span>";
+
+        //         if (textPosition++ != messageArray[0].length)
+        //             setTimeout(typewriter, speed);
+        //     }
+
+        //     window.addEventListener("load", typewriter);
     }
 });
