b77ec62fce674d8eba2f415d81a7e9f3ffc252ff
Huluette <leana.beuvant@gmail.com>
2023-09-21 10:10:07 +0200

    modifications format image et petites retouches

--- scripts/main.js
+++ scripts/main.js
@@ -6,29 +6,20 @@ const img = document.querySelector('#img');
 // MISE EN PLACE DU TEXTE PORNONCE PAR LE PERSO CHAT //
 
 const mess = document.querySelector('#mess');
-var messageArray = ["Bienvenue sur le CV de ma maîtresse !"];
-var textPosition = 0;
-var speed = 100;
 
 
 btn.addEventListener('click', () => {
     if (img.style.display == 'block') {
         img.style.display = 'none';
+
+        mess.style.display = 'none'; 
     }
     else {
         img.style.display = 'block';
 
         // MISE EN PLACE DU TEXTE PORNONCE PAR LE PERSO CHAT //
-
+        mess.style.display = 'block';
         mess.innerHTML = "Bienvenue sur le CV de ma maîtresse !";
-
-        //     typewriter = () => {
-        //         mess.innerHTML = messageArray[0].substring(0, textPosition) + "<span>\u99ae</span>";
-
-        //         if (textPosition++ != messageArray[0].length)
-        //             setTimeout(typewriter, speed);
-        //     }
-
-        //     window.addEventListener("load", typewriter);
+        
     }
 });
